a9bf0af15a7700644b2df0543e10827f89c27336
Wonjun Son <sonwoonjun@gmail.com>
2022-11-10 08:02:41 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,47 @@
+/* global $, google */
+/* global $, Swiper */
+/* global jQuery$*/
+$(window).on('load', function(){
+    $('body.fadeout').removeClass('fadeout');
+  });
+
+var posi_top, wih_half, current_view;
+$(window).scroll(function () {
+    var wih = window.innerHeight;
+    var wih_half = wih / 3;
+    current_view = $(this).scrollTop() + wih_half;
+    set_posi();
+});
+
+function set_posi() {
+
+    $(".about-title h2").each(function () {
+        var posi = $(this).offset();
+        posi_top = posi.top;
+        if (current_view+500 > posi_top) {
+            $(".about-title h2").addClass("active");
+        }
+    });
+    $(".service-title h2").each(function () {
+        var posi = $(this).offset();
+        posi_top = posi.top;
+        if (current_view+500  > posi_top) {
+            $(".service-title h2").addClass("active");
+        }
+    });
+    $(".works-title h2").each(function () {
+        var posi = $(this).offset();
+        posi_top = posi.top;
+        if (current_view+500  > posi_top) {
+            $(".works-title h2").addClass("active");
+        }
+    });
+    $(".contact-title h2").each(function () {
+        var posi = $(this).offset();
+        posi_top = posi.top;
+        if (current_view+500  > posi_top) {
+            $(".contact-title h2").addClass("active");
+        }
+    });
+    
+}
